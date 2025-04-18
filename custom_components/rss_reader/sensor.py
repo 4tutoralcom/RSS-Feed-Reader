e3c76283d69947fbf4f5df8d0f73a42f72628b2f
Andrew Dittmann <4tutoralcom@gmail.com>
2025-04-18 02:20:42 -0500

    remvoed pubdate for now to fix hulting

--- custom_components/rss_reader/sensor.py
+++ custom_components/rss_reader/sensor.py
@@ -1,44 +1,48 @@
-from homeassistant.helpers.entity import Entity
+import logging
+import feedparser
+
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.components.sensor import SensorEntity
 from .const import DOMAIN
-from .rss_feed_parser import fetch_feed_sync
+
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback):
     feeds = entry.data["feeds"]
     sensors = [RSSFeedSensor(feed_url) for feed_url in feeds]
     async_add_entities(sensors, update_before_add=True)
 
-
-class RSSFeedSensor(Entity):
+class RSSFeedSensor(SensorEntity):
     def __init__(self, feed_url):
         self._url = feed_url
-        self._attr_name = f"RSS Feed - {self._url}"
         self._state = None
-        self._attrs = {}
+        self._attr_name = f"RSS Feed: {feed_url}"
+        self._attr_unique_id = f"rss_feed_{hash(feed_url)}"
+        self._attr_extra_state_attributes = {}
 
     @property
-    def state(self):
+    def native_value(self):
         return self._state
 
-    @property
-    def extra_state_attributes(self):
-        return self._attrs
-    
-    @property
-    def should_poll(self) -> bool:
-        return True
-    
-async def async_update(self):
-    _LOGGER = logging.getLogger(__name__)
-    _LOGGER.debug("This is a debug message")
-    entry = await self.hass.async_add_executor_job(fetch_feed_sync, self._url)
-    _LOGGER.debug(entry["title"])
-    if entry:
-        self._state = entry["title"]
-        self._attrs = {
-            "guid ": entry["guid "],
-            "link": entry["link"],
-            "published": entry["published"]
-        }
+    async def async_update(self):
+        _LOGGER = logging.getLogger(__name__)
+        _LOGGER.debug("This is a debug message")
+        feed = await self.hass.async_add_executor_job(feedparser.parse, self._url)
+        _LOGGER.debug(feed.entries)
+        if feed and feed.entries:
+            self._state = "Loaded"
+            self._attr_extra_state_attributes = {
+                "entries": [
+                    {
+                        "title": entry.title,
+                        "link": entry.link,
+                        #"published": entry.pubDate,
+                        "image": entry.image,
+                    }
+                    for entry in feed.entries[:25]  # Get top 10 entries
+                ]
+            }
+        else:
+            self._state = "No entries"
+            self._attr_extra_state_attributes = {"entries": []}
