5cb59cf7e23d9c36af9ceeb5c9986a1dd7cf1a45
Andrew Dittmann <4tutoralcom@gmail.com>
2025-04-21 23:06:42 -0500

    update to use slugify function to prevent creating duplicate ids

--- custom_components/rss_reader/sensor.py
+++ custom_components/rss_reader/sensor.py
@@ -6,7 +6,7 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.components.sensor import SensorEntity
 from .const import DOMAIN
-
+from homeassistant.util import slugify
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback):
     feeds = entry.data["feeds"]
@@ -18,7 +18,7 @@ class RSSFeedSensor(SensorEntity):
         self._url = feed_url
         self._state = None
         self._attr_name = f"RSS Feed: {feed_url}"
-        self._attr_unique_id = f"rss_feed_{hash(feed_url)}"
+        self._attr_unique_id = f"rss_feed_{slugify(feed_url)}"
         self._attr_extra_state_attributes = {}
 
     @property
@@ -27,9 +27,9 @@ class RSSFeedSensor(SensorEntity):
 
     async def async_update(self):
         _LOGGER = logging.getLogger(__name__)
-        _LOGGER.debug("This is a debug message")
+        #_LOGGER.debug("This is a debug message")
         feed = await self.hass.async_add_executor_job(feedparser.parse, self._url)
-        _LOGGER.debug(feed.entries)
+        #_LOGGER.debug(feed.entries)
         if feed and feed.entries:
             self._state = "Loaded"
             self._attr_extra_state_attributes = {
@@ -37,8 +37,9 @@ class RSSFeedSensor(SensorEntity):
                     {
                         "title": entry.title,
                         "link": entry.link,
-                        #"published": entry.pubDate,
-                        "image": entry.image,
+                        "published": entry.published,
+                        "media_thumbnail": entry.media_thumbnail,
+                        "tags":entry.tags
                     }
                     for entry in feed.entries[:25]  # Get top 10 entries
                 ]
